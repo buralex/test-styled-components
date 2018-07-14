a3a6eb4ff4797b5c01f4bcb80bbd377105a62bf5
Alexandr Bur <buralex89@gmail.com>
2018-07-14 21:12:40 +0300

    delete

--- app/containers/Services/selectors.js
+++
@@ -1,8 +0,0 @@
-
-import {createSelector} from 'reselect';
-import {initialState} from './reducer';
-
-export const selectServices = state => state.services || initialState;
-
-export const makeSelectCategories = () =>
-    createSelector(selectServices, services => services.categories);
