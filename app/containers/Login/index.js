80f8402fb3285fcd9a22df781e78d73131d9d6ff
Alexandr Bur <buralex89@gmail.com>
2018-07-20 00:41:27 +0300

    axios

--- app/containers/Login/index.js
+++ app/containers/Login/index.js
@@ -36,7 +36,6 @@ import reducer from './reducer';
 import saga from './saga';
 
 import SupportForm from "./components/SupportForm";
-import LandBottom from "./components/LandBottom";
 
 import LoginForm from "./components/LoginForm";
 
@@ -102,7 +101,22 @@ class Login extends React.PureComponent {
                 </section>
 
                 <section className="land-bottom text-center">
-                    <LandBottom />
+                    <h2 className="text-truncate">About Denteez</h2>
+
+                    <div className="d-flex justify-content-between">
+                        <p>
+                            Why is it always so difficult to find what you are looking for in dentistry? Whether it is
+                            the latest advancement in technology or techniques or simply a review or
+                            understanding of the vast amount of products? Perhaps finding someone to just fix
+                            your broken equipment or simply hiring new staff or looking for that new job?
+                        </p>
+                        <p>
+                            Our mission is to give every dental professional the possibility to discuss and share all
+                            aspects of their profession, their practice and their business. We aim to make the world
+                            of dentistry easy and accessible, so every dental professional can find what they are
+                            looking for quickly and easily all in one place.
+                        </p>
+                    </div>
                 </section>
 
             </div>
