9b09617a8c7e61cf3aafd3e08814ba4c54ed9af8
Alexandr Bur <buralex89@gmail.com>
2018-07-22 01:27:33 +0300

    axios

--- app/layout/NavBar/index.js
+++ app/layout/NavBar/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import {compose} from "redux";
+import {withSignal} from "redux-signal";
+
+import {connect} from "react-redux";
+import {createStructuredSelector} from "reselect";
 
 import SearchForm from 'components/SearchForm';
-
 import Logo from 'images/logo-img.svg';
 
 import {
@@ -12,12 +16,28 @@ import {
 
 import './style.scss';
 
+
+
+import {makeSelectIsLoggedIn, makeSelectLoading, makeSelectLocation} from "../../containers/App/selectors";
+import injectReducer from "../../utils/injectReducer";
+
+import {makeSelectCurrentEnqType, makeSelectEnquiryTypes} from "../../containers/Login/selectors";
+import withData from "../../hocs/withData";
+import injectSaga from "../../utils/injectSaga";
+
+
+import * as actions from "./actions";
+import * as appActions from "../../containers/App/actions";
+
+import reducer from './reducer';
+import saga from './saga';
+
 const aaa = (values) => {
     console.log(values);
 }
 
 /* eslint-disable react/prefer-stateless-function */
-export default class NavBar extends React.PureComponent {
+class NavBar extends React.PureComponent {
     render() {
         return (
             <div className="navbar-component outline-danger">
@@ -52,3 +72,39 @@ export default class NavBar extends React.PureComponent {
     }
 }
 
+export const mapDispatchToProps = (dispatch) => ({
+    postEnquiry: (values) => dispatch(actions.postEnquiry(values)),
+    login: (values) => dispatch(appActions.login(values)),
+    showAlert: (modalName, data) => dispatch(appActions.showAlert(modalName, data)),
+
+    /* -------------------- withData hoc ---------------------------------- */
+    getData: () => {
+        dispatch(actions.loadEnquiryTypes());
+    },
+    // clearState: () => dispatch(actions.clearClientState()),
+    /* -------------------- withData hoc ---------------------------------- */
+});
+
+
+const mapStateToProps = createStructuredSelector({
+    enquiryTypes: makeSelectEnquiryTypes(),
+    currentEnqType: makeSelectCurrentEnqType(),
+    loading: makeSelectLoading(),
+    isLoggedIn: makeSelectIsLoggedIn(),
+    location: makeSelectLocation(),
+});
+
+const withConnect = connect(
+    mapStateToProps,
+    mapDispatchToProps,
+);
+
+const withReducer = injectReducer({key: 'navbar', reducer});
+const withSaga = injectSaga({key: 'navbar', saga});
+
+export default compose(
+    withReducer,
+    withSaga,
+    withSignal,
+    withConnect,
+)(withData(NavBar));
---
+++ app/layout/NavBar/reducer.js
@@ -0,0 +1,29 @@
+
+import * as types from './constants/types';
+
+export const initialState = {
+    enquiryTypes: [],
+};
+
+export default function loginReducer(state = initialState, action) {
+    switch (action.type) {
+
+        case types.LOAD_ENQUIRY_TYPES_SUCCESS: {
+            const enqTypes = action.payload.data || [];
+
+            return {
+                ...state,
+                enquiryTypes: enqTypes,
+            };
+        }
+
+        // case types.POST_ENQUIRY: {
+        //     const enqTypes = action.payload.data || [];
+        //
+        //     return state.set('enquiryTypes', enqTypes);
+        // }
+
+        default:
+            return state;
+    }
+}
